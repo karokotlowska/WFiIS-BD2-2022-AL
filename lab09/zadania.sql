17619a7859989ca2052c5591346d33dd427ebc90
karokotlowska <62810585+karokotlowska@users.noreply.github.com>
2022-05-17 19:42:38 +0200

    Create zadania.sql

---
+++ lab09/zadania.sql
@@ -0,0 +1,29 @@
+USE AdventureWorks2008
+GO
+
+
+--zad1
+EXECUTE dbo._zad1;
+GO
+
+--zad2
+EXECUTE dbo._zad2;
+GO
+
+SELECT * FROM dbo.Konta;
+
+--zad3
+EXECUTE dbo._zad3;
+GO
+
+SELECT * FROM dbo.Konta;
+
+
+--zad4
+EXECUTE dbo._zad4;
+GO
+
+SELECT * FROM dbo.Konta;
+
+
+
