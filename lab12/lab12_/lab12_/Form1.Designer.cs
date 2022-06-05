98773829af088a5bb13c2752d523252f63d75713
karokotlowska <62810585+karokotlowska@users.noreply.github.com>
2022-06-05 11:52:03 +0200

    Add files via upload

---
+++ lab12/lab12_/lab12_/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace lab12_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 17);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(264, 335);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(326, 17);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(410, 335);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(802, 17);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(345, 335);
+            this.dataGridView4.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1159, 390);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+    }
+}
+
---
+++ lab12/lab12_/lab12_/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab12_
+{
+    public partial class Form1 : Form
+    {
+        private DataClasses1DataContext db;
+
+        public Form1()
+        {
+            InitializeComponent();
+            db = new DataClasses1DataContext();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var query = from w in db.lab12_views select w;
+
+
+
+            //zad2
+            var query2 = query.GroupBy(d => new { d.DeparmentName })
+                .Select(d => new { DeparmentName = d.Key.DeparmentName, maxSalary = d.Max(row => row.Rate) })
+                .OrderByDescending(maxSalary => maxSalary.maxSalary);
+            dataGridView2.DataSource = query2;
+
+
+            //zad3
+            var query3 = query.GroupBy(v => new { v.DeparmentName })
+                .Select(v => new { DeparmentName = v.Key.DeparmentName, Rate = v.Max(row => row.Rate) })
+                .Join(query, row => new { row.DeparmentName, row.Rate }, t => new { t.DeparmentName, t.Rate }, (r, l) => new { departmentName = r.DeparmentName, employeeName = l.Name, rate = r.Rate })
+                .OrderByDescending(row => row.rate);
+            dataGridView3.DataSource = query3;
+
+
+            //zad4
+            var query4 = query.Select(d => new { name = d.Name, d.DeparmentName })
+                .Except(query.Join(db.SalesPersons, b => b.BusinessEntityID, b1 => b1.BusinessEntityID, (b, b1) => new { name = b.Name, DeparmentName = b.DeparmentName }))
+                .OrderBy(DepartmentName => DepartmentName.DeparmentName);
+            dataGridView4.DataSource = query4;
+
+
+        }
+    }
+}
