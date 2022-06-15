da56843559adad8ce19fff7391926f32f7a80d2e
karokotlowska <62810585+karokotlowska@users.noreply.github.com>
2022-06-15 09:50:36 +0200

    Add files via upload

---
+++ lab13/lab13/lab13/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(291, 31);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(258, 763);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(555, 31);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(258, 763);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(258, 763);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 824);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ lab13/lab13/lab13/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+
+namespace lab13
+{
+    public partial class Form1 : Form
+    {
+
+        private DataClasses1DataContext db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new DataClasses1DataContext();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XNamespace nsp = "http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/IndividualSurvey";
+            XNamespace ns = "http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/Resume";
+
+
+            //zad1
+            var query1 = db.Persons.Select(p => new { name = p.LastName, demographics = p.Demographics });
+
+            var val = new List<String>();
+
+            foreach (var d in query1)
+            {
+                var qry = from commutedistance in ((XElement)d.demographics).Descendants(nsp + "CommuteDistance")
+                          select commutedistance.Value;
+
+                foreach (var q in qry)
+                {
+                    char first = q[0];
+                    char second = q[2];
+                    if ((first == '1' && second == '2') || (first == '2' && second == '5'))
+                    {
+                        string res = d.name + " " + q + " ";
+                        val.Add(res);
+                    }
+                }
+            }
+
+
+
+            //zad2
+            var val2 = new List<string>();
+
+            foreach (var d in query1)
+            {
+
+                XElement elem = d.demographics;
+
+                var qry2 = from number in elem.Elements(nsp + "NumberCarsOwned")
+                           where int.Parse(number.Value) > 2
+                           select number.Value;
+
+
+                foreach (var q in qry2)
+                {
+                    string res = d.name + " " + q + " ";
+                    val2.Add(res);
+                }                   
+            }
+
+
+            //zad3
+            var query3 = db.JobCandidates.Select(j => new { resume = j.Resume });
+
+            var val3 = new List<string>();
+
+            foreach (var d in query3)
+            {
+                XElement resumeXml = d.resume;
+
+                var qry2 = from add in resumeXml.Elements(ns + "Address")
+                           select add;
+
+                string result = qry2.First().Element(ns + "Addr.Street").Value + " : " + qry2.First().Element(ns + "Addr.Location").Element(ns + "Location").Element(ns + "Loc.City").Value + " : " + qry2.First().Element(ns + "Addr.Telephone").Element(ns + "Telephone").Element(ns + "Tel.Number").Value;
+                    val3.Add(result);
+
+            }
+
+            dataGridView1.DataSource = val.ConvertAll(x => new { Value = x });
+            dataGridView2.DataSource = val2.ConvertAll(x => new { Value = x });
+            dataGridView3.DataSource = val3.ConvertAll(x => new { Value = x });
+        }
+
+
+    }
+}
